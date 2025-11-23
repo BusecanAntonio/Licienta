d828ed1b41060551bee3a6f6c6e5cac1acc18508
BusecanAntonio <xmaxxantono@yahoo.com>
2025-11-23 09:50:44 +0200

    First Commit

---
+++ src/main/java/org/example/licienta/LicientaApplication.java
@@ -0,0 +1,13 @@
+package org.example.licienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicientaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicientaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/licienta/LicientaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.licienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicientaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
